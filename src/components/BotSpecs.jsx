d6c4eddcdccc8daf74686599d15e579a82e8a773
benspector-mls <ben@marcylabschool.org>
2023-05-18 12:23:05 -0400

    removing the Link

--- src/components/BotSpecs.jsx
+++ src/components/BotSpecs.jsx
@@ -54,9 +54,10 @@ function BotSpecs() {
                 </div>
               </div>
             </div>
-            <Link><button className="ui button fluid">
+            {/* Wrap this button component in a Link */}
+            <button className="ui button fluid">
               Go Back
-            </button></Link>
+            </button>
           </div>
         </div>
       </div>
